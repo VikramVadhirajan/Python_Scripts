40e1debd3d909a506705d4d22eaa4e7a1fa70643
Vikram Vadhirajan <viva.1205@gmail.com>
2026-07-24 18:50:24 +0530

    lenovo-changes

--- 001_Conversion_scripts/CSV_Converter/csv_to_mysql.py
+++ 001_Conversion_scripts/CSV_Converter/csv_to_mysql.py
@@ -3,13 +3,13 @@ import mysql.connector
 from sqlalchemy import create_engine
 
 # Load CSV file into DataFrame
-csv_file_path = r"C:\06_Freelancing\Discussion\VIOdata.csv"  # Replace with your CSV file path
+csv_file_path = r"D:\06_Freelancing\Discussion\Chatbot\automotive_sku_data.csv" # Replace with your CSV file path
 df = pd.read_csv(csv_file_path)
 
 # Connect to MySQL database
-engine = create_engine('mysql+mysqlconnector://root:FBG_123@localhost:3306/vcdb_database')  # Replace with your credentials
+engine = create_engine('mysql+mysqlconnector://root:DViPa_2408@localhost:3306/Demonstration')  # Replace with your credentials
 
 # Load DataFrame into MySQL
-df.to_sql(name='viodata', con=engine, if_exists='replace', index=False)  # Replace with your table name
+df.to_sql(name='automotivedata', con=engine, if_exists='replace', index=False)  # Replace with your table name
 
 print('Data loaded into MySQL database successfully.')
